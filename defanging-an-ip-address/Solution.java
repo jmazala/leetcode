dd11627227ba2eec28ad1e7ae3a810ff3132e085
Jonathan Mazala <mazala.2@osu.edu>
2020-04-05 20:39:35 -0600

    1108.  Defanging an IP Address

---
+++ defanging-an-ip-address/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+  public String defangIPaddr(String address) {
+    StringBuilder builder = new StringBuilder();
+
+    for (char c : address.toCharArray()) {
+      if (c == '.') {
+        builder.append("[.]");
+      } else {
+        builder.append(c);
+      }
+    }
+
+    return builder.toString();
+  }
+}
