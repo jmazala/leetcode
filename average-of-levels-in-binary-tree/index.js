f6b23e05e249815f60bbdaa41cb3e230efec765e
Jonathan Mazala <mazala.2@osu.edu>
2020-04-18 19:59:07 -0600

    BFS in java and DFS in javascript

---
+++ average-of-levels-in-binary-tree/index.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+  const valuesPerLevel = [];
+
+  if (!root) {
+    return valuesPerLevel;
+  }
+
+  dfs(root, 0);
+
+  return valuesPerLevel.map(i => _.sum(i) / i.length);
+
+  function dfs(node, level) {
+    if (!node) {
+      return;
+    }
+
+    valuesPerLevel[level] = valuesPerLevel[level] || [];
+    valesPerLevel[level].push(node.val);
+    dfs(node.left, level + 1);
+    dfs(node.right, level + 1);
+  }
+};
