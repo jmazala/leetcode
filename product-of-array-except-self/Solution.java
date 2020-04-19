2840880988f11d90e22665205004863488e78ca8
Jonathan Mazala <mazala.2@osu.edu>
2020-04-19 17:58:18 -0600

    without extra array

--- product-of-array-except-self/Solution.java
+++ product-of-array-except-self/Solution.java
@@ -1,36 +1,66 @@
 import java.util.Arrays;
 
 class Solution {
+  // using output as left array and single int as right
   public static int[] productExceptSelf(int[] nums) {
-    int[] answer = new int[nums.length];
-    int[] left = new int[nums.length];
-    int[] right = new int[nums.length];
+    int N = nums.length;
+    if (N == 0) {
+      return nums;
+    }
 
-    //product of everything to the left of index 0 is 1
-    //as in it doesn't affect.
-    left[0] = 1;
+    int[] answer = new int[N];
+    Arrays.fill(answer, 1);
 
-    //same with right
-    right[nums.length - 1] = 1;
-
-    // fill product to the left array
+    // do product on the left
     for (int i = 1; i < nums.length; i++) {
-      left[i] = left[i - 1] * nums[i - 1];
+      answer[i] = answer[i - 1] * nums[i - 1];
     }
 
-    // fill product to the right array
-    for (int i = nums.length - 2; i >= 0; i--) {
-      right[i] = right[i + 1] * nums[i + 1];
-    }
-
-    //product at index i is product of everything else
-    //i.e. product to the left * product to the right
-    for (int i = 0; i < nums.length; i++) {
-      answer[i] = left[i] * right[i];
+    // apply right product
+    int rightProduct = 1;
+    // by starting at last element we multiply by 1, then build
+    for (int i = N - 1; i >= 0; i--) {
+      answer[i] = answer[i] * rightProduct;
+      rightProduct *= nums[i];
     }
 
     return answer;
   }
+
+  // populating left and right arrays
+  // public static int[] productExceptSelf(int[] nums) {
+  // int N = nums.length;
+  // if (N == 0) {
+  // return nums;
+  // }
+
+  // int[] answer = new int[N];
+  // int[] left = new int[N];
+  // int[] right = new int[N];
+
+  // //product of everything to the left of index 0 is 1
+  // //as in it doesn't affect.
+  // left[0] = 1;
+
+  // //same with right
+  // right[N - 1] = 1;
+
+  // // fill product to the left array
+  // // and right array also
+  // for (int i = 1; i < nums.length; i++) {
+  // left[i] = left[i - 1] * nums[i - 1];
+  // int rightI = N - 1 - i;
+  // right[rightI] = right[rightI + 1] * nums[rightI + 1];
+  // }
+
+  // //product at index i is product of everything else
+  // //i.e. product to the left * product to the right
+  // for (int i = 0; i < nums.length; i++) {
+  // answer[i] = left[i] * right[i];
+  // }
+
+  // return answer;
+  // }
 
   public static void main(String[] args) {
     System.out.println(Arrays.toString(Solution.productExceptSelf(new int[] { 1, 2, 3, 4 }))); // [24, 12, 8, 6]
