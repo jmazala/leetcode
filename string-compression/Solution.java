e735b9c699f101e4347e675bbf95cdc5e8b411ef
Jonathan Mazala <mazala.2@osu.edu>
2020-04-21 22:34:52 -0600

    better algorithm.  my algorithm.

--- string-compression/Solution.java
+++ string-compression/Solution.java
@@ -1,19 +1,39 @@
 class Solution {
   public static int compress(char[] chars) {
-    int anchor = 0, write = 0;
-    for (int read = 0; read < chars.length; read++) {
-        if (read + 1 == chars.length || chars[read + 1] != chars[read]) {
-            chars[write++] = chars[anchor];
-            if (read > anchor) {
-                for (char c: ("" + (read - anchor + 1)).toCharArray()) {
-                    chars[write++] = c;
-                }
-            }
-            anchor = read + 1;
-        }
+    if (chars == null || chars.length == 0) {
+      return 0;
     }
+
+    if (chars.length == 1) {
+      return 1;
+    }
+
+    int read = 0;
+    int write = 0;
+
+    while (read < chars.length) {
+      int count = 0;
+      char current = chars[read];
+
+      while (read < chars.length && chars[read] == current) {
+        read++;
+        count++;
+      }
+
+      chars[write] = current;
+      write++;
+
+      if (count == 1) {
+        continue;
+      }
+
+      for (char digit : String.valueOf(count).toCharArray()) {
+        chars[write++] = digit;
+      }
+    }
+
     return write;
-}
+  }
 
   public static void main(String[] args) {
     System.out.println(Solution.compress(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }));
