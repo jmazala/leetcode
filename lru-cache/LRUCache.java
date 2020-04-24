7edbd0b50070b79ca879f0416e48b3f14898c45e
Jonathan Mazala <mazala.2@osu.edu>
2020-04-24 12:33:50 -0600

    double linked list solution is better than heap for O(1)

--- lru-cache/LRUCache.java
+++ lru-cache/LRUCache.java
@@ -1,62 +1,85 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class LRUCacheItem {
   int key;
-  long lastAccessedTime;
   int value;
+  LRUCacheItem previous;
+  LRUCacheItem next;
 
-  public LRUCacheItem(int key, int value) {
-    this.value = value;
-    this.key = key;
-    updateLastAccessedTime();
+  public LRUCacheItem() {
   }
 
-  public void updateLastAccessedTime() {
-    this.lastAccessedTime = System.nanoTime();
+  public LRUCacheItem(int key, int value) {
+    this.key = key;
+    this.value = value;
   }
 }
 
-public class LRUCache {
+class LRUCache {
   int capacity;
   Map<Integer, LRUCacheItem> hash;
-  PriorityQueue<LRUCacheItem> minHeap = new PriorityQueue<>(
-      (a, b) -> Long.compare(a.lastAccessedTime, b.lastAccessedTime));
+
+  // DOUBLE LINKED LIST HEAD (MOST RECENTLY UDPATED) TAIL (LEAST RECENTLY UPDATED)
+  LRUCacheItem headDummy;
+  LRUCacheItem tailDummy;
 
   public LRUCache(int capacity) {
     this.capacity = capacity;
     this.hash = new HashMap<>();
+    this.headDummy = new LRUCacheItem();
+    this.tailDummy = new LRUCacheItem();
+    headDummy.next = tailDummy;
+    tailDummy.previous = headDummy;
   }
 
-  public void put(int key, int value) {
-    LRUCacheItem item = hash.get(key);
-    hash.remove(key);
-    if (item != null) {
-      minHeap.remove(item);
-    }
-
-    item = new LRUCacheItem(key, value);
-    hash.put(key, item);
-    minHeap.add(item);
-    if (hash.size() > capacity) {
-      LRUCacheItem itemToRemove = minHeap.remove();
-      hash.remove(itemToRemove.key);
-    }
-  }
-
-  public Integer get(int key) {
-    LRUCacheItem item = hash.get(key);
+  public int get(int key) {
+    LRUCacheItem item = this.hash.get(key);
     if (item == null) {
       return -1;
     }
 
-    // unfortunately, the heap doesn't magically re-sort itself
-    // based on updating the object in the hash
-    minHeap.remove(item);
-    item.updateLastAccessedTime();
-    minHeap.add(item);
+    moveToFront(item);
     return item.value;
+  }
+
+  private void removeFromList(LRUCacheItem item) {
+    item.previous.next = item.next;
+    item.next.previous = item.previous;
+  }
+
+  private void moveToFront(LRUCacheItem item) {
+    removeFromList(item);
+    addToFront(item);
+  }
+
+  private void addToFront(LRUCacheItem item) {
+    item.next = headDummy.next;
+    item.next.previous = item;
+    item.previous = headDummy;
+    headDummy.next = item;
+  }
+
+  public void put(int key, int value) {
+    LRUCacheItem item = this.hash.get(key);
+    if (item != null) {
+      item.value = value;
+      moveToFront(item);
+    } else {
+      item = new LRUCacheItem(key, value);
+      addToFront(item);
+    }
+
+    this.hash.put(key, item);
+
+    if (this.hash.size() > capacity) {
+      evictLeastRecentlyUsedItem();
+    }
+  }
+
+  private void evictLeastRecentlyUsedItem() {
+    LRUCacheItem itemToRemove = tailDummy.previous;
+    this.hash.remove(itemToRemove.key);
+    removeFromList(itemToRemove);
   }
 
   public static void main(String[] args) {
@@ -65,7 +88,21 @@ public class LRUCache {
     cache.put(1, 1); // ok
     cache.put(2, 3); // update key 2 to val 3
     cache.put(4, 1); // evict 1
-    System.out.println(cache.get(1)); // null
+    System.out.println(cache.get(1)); // -1
     System.out.println(cache.get(2)); // 3
+
+    LRUCache cache2 = new LRUCache(2);
+    System.out.println(cache2.get(2)); // -1
+    cache2.put(2, 6); // OK
+    System.out.println(cache2.get(1)); // -1
+    cache2.put(1, 5); // OK
+    cache2.put(1, 2); // OK
+    System.out.println(cache2.get(1)); // 2
+    System.out.println(cache2.get(2)); // 6
   }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such: LRUCache obj =
+ * new LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
+ */
