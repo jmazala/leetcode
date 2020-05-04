0b7095a08670cf371fb26815601e40bb86a8cf66
Jonathan Mazala <mazala.2@osu.edu>
2020-05-04 12:06:01 -0600

    jewels and stones again for may challenge

--- jewels-and-stones/Solution.java
+++ jewels-and-stones/Solution.java
@@ -1,26 +1,30 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
   public int numJewelsInStones(String J, String S) {
-    HashSet<Character> hash = new HashSet<>();
-    for (Character c : J.toCharArray()) {
-      hash.add(c);
+    if (S.length() == 0 || J.length() == 0) {
+      return 0;
     }
 
-    int count = 0;
-    for (Character c : S.toCharArray()) {
-      if (hash.contains(c)) {
-        count++;
+    Set<Character> set = new HashSet<>();
+    for (char jewel : J.toCharArray()) {
+      set.add(jewel);
+    }
+
+    int answer = 0;
+    for (char stone : S.toCharArray()) {
+      if (set.contains(stone)) {
+        answer++;
       }
     }
 
-    return count;
+    return answer;
   }
 
   public static void main(String[] args) {
     Solution s = new Solution();
     String J = "aA";
     String S = "aAAbbbb";
-    System.out.println(s.numJewelsInStones(J, S));
+    System.out.println(s.numJewelsInStones(J, S)); // 3
   }
 }
--- jewels-and-stones/index.js
+++ jewels-and-stones/index.js
@@ -4,17 +4,6 @@
  * @return {number}
  */
 var numJewelsInStones = function (J, S) {
-  // return Array.from(S).filter(i => {return J.indexOf(i) !== -1}).length;
-  const hash = {};
-
-  for (let i = 0; i < J.length; i++) {
-    hash[J[i]] = 1;
-  }
-
-  let count = 0;
-  for (let i = 0; i < S.length; i++) {
-    if (S[i] in hash) count++
-  }
-
-  return count;
+  const set = new Set(Array.from(J));
+  return Array.from(S).filter(i => set.has(i)).length;
 };
