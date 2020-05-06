79e271f7e496dd47f597bff5c01028ad049ec436
Jonathan Mazala <mazala.2@osu.edu>
2020-05-06 12:35:37 -0600

    use built in functions is easier

--- reverse-words-in-a-string/index.js
+++ reverse-words-in-a-string/index.js
@@ -2,40 +2,47 @@
  * @param {string} s
  * @return {string}
  */
+
+//USING BUILT IN FUNCTIONS
 var reverseWords = function (s) {
-  s = s.replace(/\s+/g, ' ').trim();
-  const array = s.split('');
-
-  //reverse entire thing
-  reverseArray(array, 0, array.length - 1);
-  let i = 0;
-  let start = i;
-  while (i < array.length) {
-    //go through and reverse word by word
-    while (array[i] !== ' ' && i < array.length) {
-      i++;
-    }
-
-    reverseArray(array, start, i - 1);
-    i++;
-    start = i;
-  }
-
-  return array.join('');
-};
-
-function reverseArray(array, start, end) {
-  // return array.slice(0, start).concat(array.slice(start, end + 1).reverse()).concat(array.slice(end + 1));
-  while (start < end) {
-    const temp = array[start];
-    array[start] = array[end];
-    array[end] = temp;
-    start++;
-    end--;
-  }
-
-  return;
+  return s.trim().split(' ').filter(i => i.length > 0).reverse().join(' ');
 }
+
+//USING LESS BUILT IN FUNCTIONS
+// var reverseWords = function (s) {
+//   s = s.replace(/\s+/g, ' ').trim();
+//   const array = s.split('');
+
+//   //reverse entire thing
+//   reverseArray(array, 0, array.length - 1);
+//   let i = 0;
+//   let start = i;
+//   while (i < array.length) {
+//     //go through and reverse word by word
+//     while (array[i] !== ' ' && i < array.length) {
+//       i++;
+//     }
+
+//     reverseArray(array, start, i - 1);
+//     i++;
+//     start = i;
+//   }
+
+//   return array.join('');
+// };
+
+// function reverseArray(array, start, end) {
+//   // return array.slice(0, start).concat(array.slice(start, end + 1).reverse()).concat(array.slice(end + 1));
+//   while (start < end) {
+//     const temp = array[start];
+//     array[start] = array[end];
+//     array[end] = temp;
+//     start++;
+//     end--;
+//   }
+
+//   return;
+// }
 
 console.log(reverseWords('the sky is blue'));
 console.log(reverseWords('  hello world!  '));
