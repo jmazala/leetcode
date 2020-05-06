c426b6aa308684baed7c34e75b38bbf989927034
Jonathan Mazala <mazala.2@osu.edu>
2020-05-06 12:23:17 -0600

    reverse words in a string, js

---
+++ reverse-words-in-a-string/index.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  s = s.replace(/\s+/g, ' ').trim();
+  const array = s.split('');
+
+  //reverse entire thing
+  reverseArray(array, 0, array.length - 1);
+  let i = 0;
+  let start = i;
+  while (i < array.length) {
+    //go through and reverse word by word
+    while (array[i] !== ' ' && i < array.length) {
+      i++;
+    }
+
+    reverseArray(array, start, i - 1);
+    i++;
+    start = i;
+  }
+
+  return array.join('');
+};
+
+function reverseArray(array, start, end) {
+  while (start < end) {
+    const temp = array[start];
+    array[start] = array[end];
+    array[end] = temp;
+    start++;
+    end--;
+  }
+
+  return;
+}
+
+// console.log(reverseWords('the sky is blue'));
+console.log(reverseWords('  hello world!  '));
+console.log(reverseWords('a good   example'));
+console.log(reverseWords('   one.   +two three?   ~four   !five- '));
