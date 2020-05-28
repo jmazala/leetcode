ab058c5cef8155636b200d1ded867add7591b86d
Jonathan Mazala <mazala.2@osu.edu>
2020-05-28 00:10:09 -0700

    stack solution for possible bipartition

--- possible-bipartition/Solution.java
+++ possible-bipartition/Solution.java
@@ -3,8 +3,9 @@ import java.util.*;
 class Solution {
   private final static int GROUP_ONE = 0;
   private final static int GROUP_TWO = 1;
-  private final static int NUM_GROUPS = 2;
+  // private final static int NUM_GROUPS = 2;
 
+  // WITH A STACK
   public boolean possibleBipartition(int N, int[][] dislikes) {
     if (dislikes == null || dislikes.length == 0 || dislikes[0].length == 0 || N == 1) {
       return true;
@@ -20,51 +21,164 @@ class Solution {
       graph.get(dislike[1]).add(dislike[0]);
     }
 
-    ArrayList<Set<Integer>> groups = new ArrayList<>();
-    for (int i = 0; i < NUM_GROUPS; i++) {
-      groups.add(new HashSet<>());
-    }
+    Integer[] groupAssignments = new Integer[N + 1];
+    Arrays.fill(groupAssignments, null);
 
     for (int i = 1; i <= N; i++) {
-      if (Solution.isAssigned(groups, i)) {
-        continue; // this person is already on a team
+      if (groupAssignments[i] != null) {
+        continue;
       }
 
-      if (!dfs(i, graph, groups, GROUP_ONE)) {
-        return false;
-      }
-    }
+      groupAssignments[i] = GROUP_ONE;
+      Stack<Integer> stack = new Stack<>();
+      stack.push(i);
 
-    return true;
-  }
-
-  private boolean dfs(int person, ArrayList<ArrayList<Integer>> graph, ArrayList<Set<Integer>> groups,
-      int groupNumber) {
-    if (Solution.isAssigned(groups, person)) {
-      return groups.get(groupNumber).contains(person);
-    }
-
-    groups.get(groupNumber).add(person);
-
-    int otherGroupNumber = groupNumber == GROUP_ONE ? GROUP_TWO : GROUP_ONE;
-    for (int dislikedPerson : graph.get(person)) {
-      if (!dfs(dislikedPerson, graph, groups, otherGroupNumber)) {
-        return false;
+      while (!stack.isEmpty()) {
+        int person = stack.pop();
+        for (int dislikedPerson : graph.get(person)) {
+          if (groupAssignments[dislikedPerson] != null) {
+            if (groupAssignments[dislikedPerson] == groupAssignments[person]) {
+              return false; // they're in the same group
+            }
+          } else {
+            groupAssignments[dislikedPerson] = groupAssignments[person] == GROUP_ONE ? GROUP_TWO : GROUP_ONE;
+            stack.push(dislikedPerson);
+          }
+        }
       }
     }
 
     return true;
   }
 
-  private static boolean isAssigned(List<Set<Integer>> teams, int person) {
-    for (int i = 0; i < NUM_GROUPS; i++) {
-      if (teams.get(i).contains(person)) {
-        return true;
-      }
-    }
+  // WITH DFS AND HASH
+  // public boolean possibleBipartition(int N, int[][] dislikes) {
+  // if (dislikes == null || dislikes.length == 0 || dislikes[0].length == 0 || N
+  // == 1) {
+  // return true;
+  // }
 
-    return false;
-  }
+  // Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
+  // for (int i = 0; i <= N; i++) {
+  // graph.put(i, new ArrayList<>());
+  // }
+
+  // for (int[] dislike : dislikes) {
+  // graph.get(dislike[0]).add(dislike[1]);
+  // graph.get(dislike[1]).add(dislike[0]);
+  // }
+
+  // Map<Integer, Set<Integer>> groups = new HashMap<>();
+  // for (int i = 0; i < NUM_GROUPS; i++) {
+  // groups.put(i, new HashSet<>());
+  // }
+
+  // for (int i = 1; i <= N; i++) {
+  // if (Solution.isAssigned(groups, i)) {
+  // continue; // this person is already on a team
+  // }
+
+  // if (!dfs(i, graph, groups, GROUP_ONE)) {
+  // return false;
+  // }
+  // }
+
+  // return true;
+  // }
+
+  // private boolean dfs(int person, Map<Integer, ArrayList<Integer>> graph,
+  // Map<Integer, Set<Integer>> groups,
+  // int groupNumber) {
+  // if (Solution.isAssigned(groups, person)) {
+  // return groups.get(groupNumber).contains(person);
+  // }
+
+  // groups.get(groupNumber).add(person);
+
+  // int otherGroupNumber = groupNumber == GROUP_ONE ? GROUP_TWO : GROUP_ONE;
+  // for (int dislikedPerson : graph.get(person)) {
+  // if (!dfs(dislikedPerson, graph, groups, otherGroupNumber)) {
+  // return false;
+  // }
+  // }
+
+  // return true;
+  // }
+
+  // private static boolean isAssigned(Map<Integer, Set<Integer>> teams, int
+  // person) {
+  // for (int i = 0; i < NUM_GROUPS; i++) {
+  // if (teams.get(i).contains(person)) {
+  // return true;
+  // }
+  // }
+
+  // return false;
+  // }
+
+  // WITH DFS AND ARRAYLIST
+  // public boolean possibleBipartition(int N, int[][] dislikes) {
+  // if (dislikes == null || dislikes.length == 0 || dislikes[0].length == 0 || N
+  // == 1) {
+  // return true;
+  // }
+
+  // ArrayList<ArrayList<Integer>> graph = new ArrayList<>(N + 1);
+  // for (int i = 0; i <= N; i++) {
+  // graph.add(new ArrayList<>());
+  // }
+
+  // for (int[] dislike : dislikes) {
+  // graph.get(dislike[0]).add(dislike[1]);
+  // graph.get(dislike[1]).add(dislike[0]);
+  // }
+
+  // ArrayList<Set<Integer>> groups = new ArrayList<>();
+  // for (int i = 0; i < NUM_GROUPS; i++) {
+  // groups.add(new HashSet<>());
+  // }
+
+  // for (int i = 1; i <= N; i++) {
+  // if (Solution.isAssigned(groups, i)) {
+  // continue; // this person is already on a team
+  // }
+
+  // if (!dfs(i, graph, groups, GROUP_ONE)) {
+  // return false;
+  // }
+  // }
+
+  // return true;
+  // }
+
+  // private boolean dfs(int person, ArrayList<ArrayList<Integer>> graph,
+  // ArrayList<Set<Integer>> groups,
+  // int groupNumber) {
+  // if (Solution.isAssigned(groups, person)) {
+  // return groups.get(groupNumber).contains(person);
+  // }
+
+  // groups.get(groupNumber).add(person);
+
+  // int otherGroupNumber = groupNumber == GROUP_ONE ? GROUP_TWO : GROUP_ONE;
+  // for (int dislikedPerson : graph.get(person)) {
+  // if (!dfs(dislikedPerson, graph, groups, otherGroupNumber)) {
+  // return false;
+  // }
+  // }
+
+  // return true;
+  // }
+
+  // private static boolean isAssigned(List<Set<Integer>> teams, int person) {
+  // for (int i = 0; i < NUM_GROUPS; i++) {
+  // if (teams.get(i).contains(person)) {
+  // return true;
+  // }
+  // }
+
+  // return false;
+  // }
 
   public static void main(String[] args) {
     Solution s = new Solution();
@@ -73,5 +187,6 @@ class Solution {
     System.out.println(s.possibleBipartition(5, new int[][] { { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 } })); // true
     System.out.println(s.possibleBipartition(3, new int[][] { { 1, 2 }, { 1, 3 }, { 2, 3 } })); // false
     System.out.println(s.possibleBipartition(5, new int[][] { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 1, 5 } })); // false
+    System.out.println(s.possibleBipartition(5, new int[][] { { 1, 2 }, { 3, 4 }, { 4, 5 }, { 3, 5 } })); // false
   }
 }
--- possible-bipartition/index.js
+++ possible-bipartition/index.js
@@ -4,12 +4,12 @@
  * @return {boolean}
  */
 
-//WITH A GRAPH AND DFS
+//WITH A FOR LOOP
 var possibleBipartition = function (N, dislikes) {
   if (N === 1 || dislikes.length === 0) {
     return true;
   }
-  
+
   const dislikeHash = Array(N + 1).fill().map(i => Array(0).fill());
 
   //each dislike is an EDGE on the GRAPH we can't traverse
@@ -23,34 +23,76 @@ var possibleBipartition = function (N, dislikes) {
 
   for (let i = 1; i <= N; i++) {
     if (i in groupAssignments) {
-      continue; //already been assigned to a group from the DFS
+      continue;
     }
 
-    if (!dfs(i, 0)) {
-      return false;
+    groupAssignments[i] = 0;
+    const stack = [i];
+    while (stack.length) {
+      const person = stack.pop();
+      for (dislikedPerson of dislikeHash[person]) {
+        if (dislikedPerson in groupAssignments) {
+          if (groupAssignments[dislikedPerson] === groupAssignments[person]) {
+            return false;
+          }
+        } else {
+          groupAssignments[dislikedPerson] = groupAssignments[person] === 0 ? 1 : 0;
+          stack.push(dislikedPerson);
+        }
+      }
     }
   }
 
   return true;
-
-  function dfs(person, groupNumber) {
-    if (person in groupAssignments) {
-      return groupAssignments[person] === groupNumber;
-    }
-
-    groupAssignments[person] = groupNumber;
-
-    const dislikedGroupNumber = groupNumber === 0 ? 1 : 0;
-    for (const dislikedPerson of dislikeHash[person]) {
-      //try and put them in the other group with the dfs call
-      if (!dfs(dislikedPerson, dislikedGroupNumber)) {
-        return false;
-      }
-    }
-
-    return true;
-  }
 }
+
+//WITH A GRAPH AND DFS
+// var possibleBipartition = function (N, dislikes) {
+//   if (N === 1 || dislikes.length === 0) {
+//     return true;
+//   }
+
+//   const dislikeHash = Array(N + 1).fill().map(i => Array(0).fill());
+
+//   //each dislike is an EDGE on the GRAPH we can't traverse
+//   for ([person1, person2] of dislikes) {
+//     dislikeHash[person1].push(person2);
+//     dislikeHash[person2].push(person1);
+//   }
+
+//   //groupAssignments[i] = GROUP 0 or GROUP 1 for person i
+//   const groupAssignments = {};
+
+//   for (let i = 1; i <= N; i++) {
+//     if (i in groupAssignments) {
+//       continue; //already been assigned to a group from the DFS
+//     }
+
+//     if (!dfs(i, 0)) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+
+//   function dfs(person, groupNumber) {
+//     if (person in groupAssignments) {
+//       return groupAssignments[person] === groupNumber;
+//     }
+
+//     groupAssignments[person] = groupNumber;
+
+//     const dislikedGroupNumber = groupNumber === 0 ? 1 : 0;
+//     for (const dislikedPerson of dislikeHash[person]) {
+//       //try and put them in the other group with the dfs call
+//       if (!dfs(dislikedPerson, dislikedGroupNumber)) {
+//         return false;
+//       }
+//     }
+
+//     return true;
+//   }
+// }
 
 //recursive iterations, trying all combinations.  time limit exceeded
 // var possibleBipartition = function (N, dislikes) {
@@ -110,6 +152,7 @@ var possibleBipartition = function (N, dislikes) {
 //   }
 // };
 
+console.log(possibleBipartition(50, [[21, 47], [4, 41], [2, 41], [36, 42], [32, 45], [26, 28], [32, 44], [5, 41], [29, 44], [10, 46], [1, 6], [7, 42], [46, 49], [17, 46], [32, 35], [11, 48], [37, 48], [37, 43], [8, 41], [16, 22], [41, 43], [11, 27], [22, 44], [22, 28], [18, 37], [5, 11], [18, 46], [22, 48], [1, 17], [2, 32], [21, 37], [7, 22], [23, 41], [30, 39], [6, 41], [10, 22], [36, 41], [22, 25], [1, 12], [2, 11], [45, 46], [2, 22], [1, 38], [47, 50], [11, 15], [2, 37], [1, 43], [30, 45], [4, 32], [28, 37], [1, 21], [23, 37], [5, 37], [29, 40], [6, 42], [3, 11], [40, 42], [26, 49], [41, 50], [13, 41], [20, 47], [15, 26], [47, 49], [5, 30], [4, 42], [10, 30], [6, 29], [20, 42], [4, 37], [28, 42], [1, 16], [8, 32], [16, 29], [31, 47], [15, 47], [1, 5], [7, 37], [14, 47], [30, 48], [1, 10], [26, 43], [15, 46], [42, 45], [18, 42], [25, 42], [38, 41], [32, 39], [6, 30], [29, 33], [34, 37], [26, 38], [3, 22], [18, 47], [42, 48], [22, 49], [26, 34], [22, 36], [29, 36], [11, 25], [41, 44], [6, 46], [13, 22], [11, 16], [10, 37], [42, 43], [12, 32], [1, 48], [26, 40], [22, 50], [17, 26], [4, 22], [11, 14], [26, 39], [7, 11], [23, 26], [1, 20], [32, 33], [30, 33], [1, 25], [2, 30], [2, 46], [26, 45], [47, 48], [5, 29], [3, 37], [22, 34], [20, 22], [9, 47], [1, 4], [36, 46], [30, 49], [1, 9], [3, 26], [25, 41], [14, 29], [1, 35], [23, 42], [21, 32], [24, 46], [3, 32], [9, 42], [33, 37], [7, 30], [29, 45], [27, 30], [1, 7], [33, 42], [17, 47], [12, 47], [19, 41], [3, 42], [24, 26], [20, 29], [11, 23], [22, 40], [9, 37], [31, 32], [23, 46], [11, 38], [27, 29], [17, 37], [23, 30], [14, 42], [28, 30], [29, 31], [1, 8], [1, 36], [42, 50], [21, 41], [11, 18], [39, 41], [32, 34], [6, 37], [30, 38], [21, 46], [16, 37], [22, 24], [17, 32], [23, 29], [3, 30], [8, 30], [41, 48], [1, 39], [8, 47], [30, 44], [9, 46], [22, 45], [7, 26], [35, 42], [1, 27], [17, 30], [20, 46], [18, 29], [3, 29], [4, 30], [3, 46]])); //true
 console.log(possibleBipartition(99, [])); //true
 console.log(possibleBipartition(4, [[1, 2], [1, 3], [2, 4]])); //true group1=[1,4] group2=[2,3]
 console.log(possibleBipartition(5, [[1, 2], [1, 3], [1, 4], [1, 5]])); // true
