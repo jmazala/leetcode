ec52fae850a5fdedf453f5e0635bc37b2b75dea7
Jonathan Mazala <mazala.2@osu.edu>
2020-06-01 10:21:54 -0700

    leetcode add binary.  popular facebook question

---
+++ add-binary/index.js
@@ -0,0 +1,57 @@
+const _ = require('lodash');
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+
+//THIS DOESNT WORK BECAUSE OVERFLOWS
+// var addBinary = function(a, b) {
+//     return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
+// };
+
+var addBinary = function(a, b) {
+  const maxLength = Math.max(a.length, b.length);
+  a = _.padStart(a, maxLength, '0');
+  b = _.padStart(b, maxLength, '0');
+  
+  let carry = false;
+  let answer = [];
+  
+  for (let i = maxLength - 1; i >= 0; i--) {
+    const x = a[i];
+    const y = b[i];
+    
+    if (x === '0' && y === '0') {
+      if (carry) {
+        answer.unshift(1);
+      } else {
+        answer.unshift(0);
+      }
+      carry = false;
+    } else if (x ^ y) { //only one is '1'
+      if (carry) {
+        answer.unshift(0);
+      } else {
+        answer.unshift(1);
+      }
+    } else { //they are both '1'
+      if (carry) {
+        answer.unshift(1);
+      } else {
+        answer.unshift(0);
+      }
+
+      carry = true;
+    }
+  }
+
+  if (carry) {
+    answer.unshift(1);
+  }
+  
+  return answer.join('');
+}
+
+console.log(addBinary('11', '1')); //100
