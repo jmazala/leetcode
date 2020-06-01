ec02a96a387c6fb87b4342f185d180af81619010
Jonathan Mazala <mazala.2@osu.edu>
2020-06-01 10:06:52 -0700

    using map in stead of hash to preserve insertion order.  learned something new today

--- longest-substring-with-at-most-k-distinct-characters/index.js
+++ longest-substring-with-at-most-k-distinct-characters/index.js
@@ -3,6 +3,13 @@
  * @param {number} k
  * @return {number}
  */
+
+ //WITH A MAP OBJECT (YES, JAVASCRIPT MAP PRESERVES INSERTION ORDER)
+ /**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
 var lengthOfLongestSubstringKDistinct = function (s, k) {
   if (k === 0 || s.length === 0) {
     return 0;
@@ -11,38 +18,75 @@ var lengthOfLongestSubstringKDistinct = function (s, k) {
   let left = 0;
   let right = 0;
   let answer = 0;
-  let substring = [];
-  let hash = {};
+  let hash = new Map();
 
   while (right < s.length) {
-    const c = s[right++];
-    substring.push(c);
-    hash[c] = hash[c] || 0;
-    hash[c]++;
-
-    while (hashSize() > k) {
-      const toRemove = s[left];
-      hash[toRemove]--;
-
-      if (hash[toRemove] === 0) {
-        delete hash[toRemove];
-      }
-
-      substring.shift();
-      left++;
+    const c = s[right];
+    
+    /*
+     * it's faster to check if the key is in the map
+     * rather than just delete it by default
+     */
+    if (hash.has(c)) {
+      hash.delete(c);
     }
-
-    if (hashSize() <= k && substring.length > answer) {
-      answer = substring.length;
+    
+    hash.set(c, right);
+    right++;
+    
+    if (hash.size > k) {
+      const [leftmostChar, itsLastPosition] = hash.entries().next().value;
+      hash.delete(leftmostChar);
+      left = itsLastPosition + 1;
     }
+    
+    answer = Math.max(answer, right - left);
   }
 
   return answer;
-
-  function hashSize() {
-    return Object.keys(hash).length;
-  }
 };
+ 
+//WITH AN OBJECT FOR HASH / CHAR COUNTS
+// var lengthOfLongestSubstringKDistinct = function (s, k) {
+//   if (k === 0 || s.length === 0) {
+//     return 0;
+//   }
+
+//   let left = 0;
+//   let right = 0;
+//   let answer = 0;
+//   let substring = [];
+//   let hash = {};
+
+//   while (right < s.length) {
+//     const c = s[right++];
+//     substring.push(c);
+//     hash[c] = hash[c] || 0;
+//     hash[c]++;
+
+//     while (hashSize() > k) {
+//       const toRemove = s[left];
+//       hash[toRemove]--;
+
+//       if (hash[toRemove] === 0) {
+//         delete hash[toRemove];
+//       }
+
+//       substring.shift();
+//       left++;
+//     }
+
+//     if (hashSize() <= k && substring.length > answer) {
+//       answer = substring.length;
+//     }
+//   }
+
+//   return answer;
+
+//   function hashSize() {
+//     return Object.keys(hash).length;
+//   }
+// };
 
 console.log(lengthOfLongestSubstringKDistinct('abcde', 0)); // 0
 console.log(lengthOfLongestSubstringKDistinct('', 99)); //0
