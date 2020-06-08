2e1ee2cb49e57d94ba108855a463e0e7f969ffa0
Jonathan Mazala <mazala.2@osu.edu>
2020-06-08 16:59:09 -0700

    leetcode valid palindrome ii

---
+++ valid-palindrome-ii/index.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const validPalindrome = function (s, tryAgain = true) {
+  let start = 0;
+  let end = s.length - 1;
+
+  while (start < end) {
+    if (s[start] !== s[end]) {
+      if (!tryAgain) {
+        return false;
+      }
+
+      return (
+        validPalindrome(s.slice(start, end), false) ||
+        validPalindrome(s.slice(start + 1, end + 1), false)
+      );
+    }
+
+    start++;
+    end--;
+  }
+
+  return true;
+};
+
+console.log(validPalindrome('')); // true
+console.log(validPalindrome('aa')); // true
+console.log(validPalindrome('aac')); // true
+console.log(validPalindrome('aba')); // true
+console.log(validPalindrome('abca')); // true
+console.log(validPalindrome('abeea')); // true
+console.log(validPalindrome('aacc')); // false
+console.log(validPalindrome('abeefa')); // false
