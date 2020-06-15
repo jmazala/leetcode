24bcf2a16660e14c159a18024ab8d5cf4f21cb95
Jonathan Mazala <mazala.2@osu.edu>
2020-06-15 16:43:00 -0600

    done playing with java

--- design-circular-queue/MyCircularQueue.java
+++ design-circular-queue/MyCircularQueue.java
@@ -49,12 +49,12 @@ public class MyCircularQueue {
     if (numValues > 0) {
       head = (head + 1) % values.length;
     }
-    
+
     return true;
   }
 
   /** Get the front item from the queue. */
-  public int Front() {
+  public int front() {
     if (isEmpty()) {
       return -1;
     }
@@ -63,7 +63,7 @@ public class MyCircularQueue {
   }
 
   /** Get the last item from the queue. */
-  public int Rear() {
+  public int rear() {
     if (isEmpty()) {
       return -1;
     }
@@ -81,17 +81,24 @@ public class MyCircularQueue {
     return numValues == values.length;
   }
 
+  /**
+   * runs the file lol.
+   * 
+   * @param args args
+   */
   public static void main(String[] args) {
-    // MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
+    // MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be
+    // 3
     // System.out.println(circularQueue.enQueue(1)); // return true
     // System.out.println(circularQueue.enQueue(2)); // return true
     // System.out.println(circularQueue.enQueue(3)); // return true
-    // System.out.println(circularQueue.enQueue(4)); // return false, the queue is full
-    // System.out.println(circularQueue.Rear()); // return 3
+    // System.out.println(circularQueue.enQueue(4)); // return false, the queue is
+    // full
+    // System.out.println(circularQueue.rear()); // return 3
     // System.out.println(circularQueue.isFull()); // return true
     // System.out.println(circularQueue.deQueue()); // return true
     // System.out.println(circularQueue.enQueue(4)); // return true
-    // System.out.println(circularQueue.Rear()); // return 4
+    // System.out.println(circularQueue.rear()); // return 4
 
     MyCircularQueue q2 = new MyCircularQueue(81);
     System.out.println(q2.enQueue(69)); // return true
@@ -101,7 +108,7 @@ public class MyCircularQueue {
     System.out.println(q2.deQueue()); // return true
     System.out.println(q2.isFull()); // return false
     System.out.println(q2.isFull()); // return false
-    System.out.println(q2.Front()); // return 12
+    System.out.println(q2.front()); // return 12
   }
 }
 
