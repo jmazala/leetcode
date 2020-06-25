6c3569d55e111a6ba3b29f2d0b5f119be3824f46
Jonathan Mazala <mazala.2@osu.edu>
2020-06-25 12:18:10 -0600

    more comments to explain this problem

--- find-the-duplicate-number/index.js
+++ find-the-duplicate-number/index.js
@@ -11,13 +11,24 @@ const findDuplicate = function (nums) {
   let tortoise = nums[0];
   let hare = nums[nums[0]];
 
+  // fast pointer is going 2 steps
+  // slow pointer is going 1 step
+
+  // fast pointer is going to hit the cycle faster than the slow pointer
+  // once the fast pointer is in the cycle,
   while (tortoise !== hare) {
     tortoise = nums[tortoise];
     hare = nums[nums[hare]];
   }
 
+  // reset the slow pointer to the beginning of the list
   tortoise = 0;
 
+  // hare is where that cycle spot was
+  // since the hare took 2x to reach the cycle spot
+  // the tortoise reset to
+  // and hare now going at the same speed
+  // they'll meet in the middle
   while (tortoise !== hare) {
     tortoise = nums[tortoise];
     hare = nums[hare];
