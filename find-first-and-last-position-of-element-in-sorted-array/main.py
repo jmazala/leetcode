be775aa628cb9ef5972fdb83273267f07ef14a50
Jonathan Mazala <mazala.2@osu.edu>
2020-06-26 10:23:54 -0600

    facebook leetcode sorting + searching

--- find-first-and-last-position-of-element-in-sorted-array/main.py
+++ find-first-and-last-position-of-element-in-sorted-array/main.py
@@ -1,33 +1,35 @@
 from typing import List
 import math
 
+
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-      answer = [-1, -1]
+        answer = [-1, -1]
 
-      left = binarySearch(nums, target, True)
+        left = binarySearch(nums, target, True)
 
-      if (left == len(nums) or nums[left] != target):
-        return answer
-      
-      right = binarySearch(nums, target, False)
-      return [left, right - 1]
-    
+        if (left == len(nums) or nums[left] != target):
+            return answer
+
+        right = binarySearch(nums, target, False)
+        return [left, right - 1]
+
+
 def binarySearch(nums: List[int], target: int, goLeft: bool) -> int:
-  low = 0
-  high = len(nums)
+    low = 0
+    high = len(nums)
 
-  while (low < high):
-    mid = math.floor((high + low) / 2)
-    if (nums[mid] == target and goLeft) or nums[mid] > target:
-      high = mid
-    else:
-      low = mid + 1
-  
-  return low
+    while (low < high):
+        mid = math.floor((high + low) / 2)
+        if (nums[mid] == target and goLeft) or nums[mid] > target:
+            high = mid
+        else:
+            low = mid + 1
+
+    return low
+
 
 if __name__ == '__main__':
-  s = Solution()
-  print(s.searchRange([5, 7, 7, 8, 8, 10], 8)) # [3,4]
-  print(s.searchRange([5, 7, 7, 8, 8, 10], 6)) # [-1, -1]
-        
+    s = Solution()
+    print(s.searchRange([5, 7, 7, 8, 8, 10], 8))  # [3,4]
+    print(s.searchRange([5, 7, 7, 8, 8, 10], 6))  # [-1, -1]
--- first-bad-version/main.py
+++ first-bad-version/main.py
@@ -3,34 +3,35 @@
 # @return a bool
 # def isBadVersion(version):
 
-class Solution:
-  def __init__(self, badVersion: int):
-    self.badVersion = badVersion
-    return
-  
-  def __isBadVersion(self, version: int) -> bool:
-    return version == self.badVersion
 
-  def firstBadVersion(self, n: int) -> int:
-    """
-    :type n: int
-    :rtype: int
-    """
-    #binary search for the bad version
-    low = 1
-    high = n
-    
-    while (low < high):
-      mid = low + (high - low) // 2
-      if (self.__isBadVersion(mid)):
-        high = mid
-      else:
-        low = mid + 1
-    
-    return low
+class Solution:
+    def __init__(self, badVersion: int):
+        self.badVersion = badVersion
+        return
+
+    def __isBadVersion(self, version: int) -> bool:
+        return version == self.badVersion
+
+    def firstBadVersion(self, n: int) -> int:
+        """
+        :type n: int
+        :rtype: int
+        """
+        # binary search for the bad version
+        low = 1
+        high = n
+
+        while (low < high):
+            mid = low + (high - low) // 2
+            if (self.__isBadVersion(mid)):
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
 
 
 if __name__ == '__main__':
-  for i in range(1, 7):
-    s = Solution(i)
-    print(s.firstBadVersion(i)) #i
+    for i in range(1, 7):
+        s = Solution(i)
+        print(s.firstBadVersion(i))  # i
