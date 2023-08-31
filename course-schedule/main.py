15362c9e94e0d8934e6f22f7ada89e7c5b21b257
Jonathan Mazala <mazala.2@osu.edu>
2023-08-31 12:47:05 -0700

    graph probs

--- course-schedule/main.py
+++ course-schedule/main.py
@@ -1,45 +1,56 @@
+# https://leetcode.com/problems/course-schedule/
+
 from typing import List
 
+
 class Solution:
-  def dfsHasCycle(self, course, edges, visited, recursionStack):
-    if recursionStack[course]:
-      return True
-    
-    if visited[course]:
-      return False
-    
-    visited[course] = True
-    recursionStack[course] = True
+    # METHOD 1 - CYCLE DETECTION WITH DFS
+    # Create a directed graph representation using the prereqs array
+    # For each node (course), DFS
+    # If, starting with that node, we visit the same node twice, there is a cycle
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # make a graph
+        # graph[i] = [e1, e2, ...en] leading to node i
+        edges = [[] for _ in range(numCourses)]
 
-    for neighbor in edges[course]:
-      if self.dfsHasCycle(neighbor, edges, visited, recursionStack):
+        # add edges
+        for [course, prereq] in prerequisites:
+            edges[prereq].append(course)
+
+        visited = [False] * numCourses
+        recursionStack = [False] * numCourses
+
+        for course in range(numCourses):
+            if visited[course]:
+                continue
+
+            if self.dfsHasCycle(course, edges, visited, recursionStack):
+                return False
+
         return True
-      
-    recursionStack[course] = False
-    return False
-  
-  def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-    #make a graph
-    #graph[i] = [e1, e2, ...en] leading to node i
-    edges = [[] for _ in range(numCourses)]
 
-    #add edges
-    for [course, prereq] in prerequisites:
-      edges[course].append(prereq)
-    
-    visited = [False] * numCourses
-    recursionStack = [False] * numCourses
+    def dfsHasCycle(self, course, edges, visited, recursionStack):
+        if recursionStack[course]:
+            return True
 
-    for course in range(numCourses):
-      if visited[course]:
-        continue
+        if visited[course]:
+            return False
 
-      if self.dfsHasCycle(course, edges, visited, recursionStack):
+        visited[course] = True
+
+        # Since DFS, set node as visited here (w.r.t starting node), undo later
+        recursionStack[course] = True
+
+        for neighbor in edges[course]:
+            if self.dfsHasCycle(neighbor, edges, visited, recursionStack):
+                return True
+
+        # Undo the visit, since DFS
+        recursionStack[course] = False
         return False
-    
-    return True
 
-if __name__ == '__main__':
-  s = Solution()
-  print(s.canFinish(2, [[1,0]])) #True
-  print(s.canFinish(2, [[1,0], [0, 1]])) #False
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.canFinish(2, [[1, 0]]))  # True
+    print(s.canFinish(2, [[1, 0], [0, 1]]))  # False
